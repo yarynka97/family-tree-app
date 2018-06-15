ef9d234e328665b9220925026f139f73c085df2c
Yaryna Shyika <pinkpoppy.art@gmail.com>
2018-06-15 11:39:33 +0300

    changed callbacks to async/await style

--- server/routes/trees.js
+++ server/routes/trees.js
@@ -4,44 +4,33 @@ const config = require('../../config');
 
 const User = require('../db/user.js');
 
-var treeCollection;
-var connected = false;
-
-MongoClient.connect(config.db_url, (err, client) => {
-    if (err) return console.log(err)
-    treeCollection = client.db('family-tree-app').collection('trees');
-    connected = true;
-    console.log('Connected to DB');
-})
-
 //add new tree to database
-router.post('/', (req, res) => {
-  if(connected){
-    treeCollection.save(req.body.tree, (err, tree) => {
-        if (err) {
-            res.status(500).send('Server couldn\'t save this tree');
-            return console.log(err);
-        }
-        User.findOne({ 'username': req.body.username }, (err, user) => {
-              if (err) return res.status(500).send("There was a problem finding the user.");
-              if (!user) return res.status(404).send("No user found.");
-              var trees = user.trees.push({
+router.post('/',  async (req, res) => {
+  const db = await MongoClient.connect(config.db_url);
+  const treeCollection = await db('family-tree-app').collection('trees');
+
+  try{
+    const user = await User.findOne({ 'username': req.body.username });
+    if (!user) return res.status(404).send("No user found.");
+    var trees = user.trees.push({
                 treename: tree.treename,
                 id: tree._id
               });
-              User.findOneAndUpdate({ 'username': req.body.username }, { $set: trees }, { new: true }, (err, updatedUser) => {
-                  if (err) return res.status(500).send("There was a problem finding the user.");
-                  if(!updatedUser) return res.status(500).send("There was a problem with updating user info.");
-                  res.status(200).send(updatedUser);
-                });
-          });
-  } else {
-    console.log('no db connection');
+    const updatedUser = await User.findOneAndUpdate({ 'username': req.body.username }, { $set: trees }, { new: true });
+    if(!updatedUser) return res.status(500).send("There was a problem with updating user info.");
+    const newTree = treeCollection.save(req.body.tree);
+    res.status(200).send(updatedUser);
+  } catch (err) {
+    return res.status(500).send("There was a problem with adding a tree to database");
   }
+  db.close();
 });
 
 //get tree by id
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  const db = await MongoClient.connect(config.db_url);
+  const treeCollection = await db('family-tree-app').collection('trees');
+
   treeCollection.findOne({ _id: ObjectId(req.params.id) }, (err, tree) => {
     if(err) return res.status(500).send(`Server couldn\'t proccess request to database`);
     if(!tree) return res.status(404).send('Tree with that id doesn\'t exist');
@@ -50,7 +39,10 @@ router.get('/:id', (req, res) => {
 });
 
 //update tree
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
+  const db = await MongoClient.connect(config.db_url);
+  const treeCollection = await db('family-tree-app').collection('trees');
+
   treeCollection.findOneAndUpdate({ _id: ObjectId(req.params.id) }, { $set: req.body }, { new: true }, (err, tree) => {
     if (err) return res.status(500).send("There was a problem finding the tree.");
     if(!tree) return res.status(500).send("There was a problem with updating tree info.");
@@ -59,24 +51,29 @@ router.put('/:id', (req, res) => {
 });
 
 //delete tree
-router.delete('/:username/:treeId', (req, res) => {
-  treeCollection.deleteOne({ _id: ObjectId(req.params.treeId) }, (err, tree) => {
-    if(err) return res.status(500).send('Server couldn\'t delete tree');
-    if(!tree) return res.status(404).send('Server couldn\'t delete tree, because no one was found');
-    User.findOne({ 'username': req.params.username }, (err, user) => {
-          if (err) return res.status(500).send("There was a problem finding the user.");
-          if (!user) return res.status(404).send("No user found.");
-          var treeIndex = user.trees.indexOf( t => t._id == tree._id );
-          treeIndex == -1 ?
-            return res.status(404).send("This user doesn\'t own this tree") :
-            var trees = user.tree.splice(treeIndex, 1);
-          User.findOneAndUpdate({ 'username': req.params.username }, { $set: trees }, { new: true }, (err, updatedUser) => {
-                if (err) return res.status(500).send("There was a problem finding the user.");
-                if(!updatedUser) return res.status(500).send("There was a problem with updating user info.");
-                res.status(200).send(updatedUser);
-              });
-        });
-  })
+router.delete('/:username/:treeId', async (req, res) => {
+  const db = await MongoClient.connect(config.db_url);
+  const treeCollection = await db('family-tree-app').collection('trees');
+
+  try{
+    const deletedTree = treeCollection.deleteOne({ _id: ObjectId(req.params.treeId) });
+    if(!deletedTree) return res.status(404).send('Server couldn\'t delete tree, because no one was found');
+
+    const user = await User.findOne({ 'username': req.params.username });
+    if (!user) return res.status(404).send("No user found.");
+    var treeIndex = user.trees.indexOf( t => t._id == tree._id );
+    if(treeIndex == -1){
+      return res.status(404).send("This user doesn\'t own this tree")
+    } else{
+      var trees = user.tree.splice(treeIndex, 1);
+    }      
+
+    const updatedUser = await User.findOneAndUpdate({ 'username': req.params.username }, { $set: trees }, { new: true });
+    if(!updatedUser) return res.status(500).send("There was a problem with updating user info.");
+    res.status(200).send(updatedUser);
+  } catch (err){
+    if(err) return res.status(500).send('Server couldn\'t delete tree from database');
+  }
 });
 
 module.exports = router;
--- server/routes/users.js
+++ server/routes/users.js
@@ -6,41 +6,46 @@ const config = require('../../config');
 const verifyToken = require('../VerifyToken');
 
 // register new user
-router.post('/', (req, res) => {
-  User.create({
-    username : req.body.username,
-    email : req.body.email,
-    password : req.body.password,
-    firstName : req.body.firstName || '',
-    lastName : req.body.lastName || '',
-    trees: req.body.trees || []
-  }, (err, user) => {
-      if (err) return res.status(500).send("There was a problem registering the user`.");
-
-      var token = jwt.sign({ id: user._id }, config.secret, {
-        expiresIn: 86400 // 24 hours
-      });
-
-      res.status(200).send({ auth: true, token });
+router.post('/', async (req, res) => {
+  try{
+    const user = await User.create({
+      username : req.body.username,
+      email : req.body.email,
+      password : req.body.password,
+      firstName : req.body.firstName || '',
+      lastName : req.body.lastName || '',
+      trees: req.body.trees || []
     });
+    const token = jwt.sign({ id: user._id }, config.secret, {
+      expiresIn: 86400 // 24 hours
+    });
+
+    res.status(200).send({ auth: true, token });
+  } catch (err) {
+   return res.status(500).send("There was a problem registering the user`.");
+  }
 });
 
 //get existing user
-router.get('/:id', verifyToken, (req, res) => {
-  User.findOne({ 'username': req.params.id }, function (err, user) {
-        if (err) return res.status(500).send("There was a problem finding the user.");
-        if (!user) return res.status(404).send("No user found.");
-        res.status(200).send(user);
-    });
+router.get('/:id', verifyToken, async (req, res) => {
+ try{
+  const user = await User.findOne({ 'username': req.params.id });
+  if (!user) return res.status(404).send("No user found.");
+  res.status(200).send(user);
+ } catch (err){
+  return res.status(500).send("There was a problem finding the user.");
+ }
 });
 
 //update user info
-router.put('/:id', verifyToken, (req,res)=>{
-  User.findOneAndUpdate({ 'username': req.params.id }, { $set: req.body }, { new: true }, function(err, user){
-    if (err) return res.status(500).send("There was a problem finding the user.");
-    if(!user) return res.status(500).send("There was a problem with updating user info.");
+router.put('/:id', verifyToken, async (req,res)=>{
+  try{
+    const user = await User.findOneAndUpdate({ 'username': req.params.id }, { $set: req.body }, { new: true });
+    if (!user) return res.status(404).send("No user found.");
     res.status(200).send(user);
-  });
+  } catch (err){
+    return res.status(500).send("There was a problem with updating user info.");
+  }
 })
 
 module.exports = router;
